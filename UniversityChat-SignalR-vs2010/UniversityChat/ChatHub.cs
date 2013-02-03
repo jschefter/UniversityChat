9a3112b155b82608b79adc0e3c26c3b146fcc0c7
Darren Korman <darrenk@u.washington.edu>
2013-02-02 20:50:06 -0800

    source for the SignalR prototype.  This is based of the following tutorial: http://www.asp.net/signalr/overview/getting-started/tutorial-getting-started-with-signalr

---
+++ UniversityChat-SignalR-vs2010/UniversityChat/ChatHub.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+
+
+namespace UniversityChat
+{
+    public class ChatHub : Hub
+    {
+        
+        public override Task OnConnected()
+        {
+            return base.OnDisconnected();
+        }
+
+        public override Task OnDisconnected()
+        {
+            string username = ConnectedUsers.RemoveUser(Context.ConnectionId);
+            Clients.All.broadcastMessageToChat(username, "left the chat");
+            Clients.All.setUserList(ConnectedUsers.GetConnectedUsers());
+            return base.OnDisconnected();
+        }
+
+        public override Task OnReconnected()
+        {
+            return base.OnReconnected();
+        }
+
+        public void Send(string username, string message)
+        {
+            Clients.All.broadcastMessageToChat(username, message);
+        }
+
+        public void NewUser(string username)
+        {
+            ConnectedUsers.AddUser(Context.ConnectionId, username);
+            Clients.All.broadcastMessageToChat(username, "joined the chat");
+            Clients.All.setUserList(ConnectedUsers.GetConnectedUsers());
+        }
+    }
+}
---
+++ UniversityChat-SignalR-vs2010/UniversityChat/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UniversityChat.Global" Language="C#" %>
