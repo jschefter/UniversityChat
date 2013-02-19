60aa82cb1e60580c27805b966c76fbac5dd839b4
ahmsal1984 <ahmsal19@uw.edu>
2013-02-18 21:05:06 -0800

    Added DataAccess layer initial implementation.

---
+++ UniversityChat-SignalR-vs2010/UniversityChat.Data/DataAccess/GenericDataAccess.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+
+public static class GenericDataAccess
+{    
+    static GenericDataAccess()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    // executes a command and returns the results as a DataTable object
+    public static DataTable ExecuteSelectCommand(DbCommand command)
+    {       
+        DataTable table;
+        
+        try
+        {
+            command.Connection.Open();
+            
+            DbDataReader reader = command.ExecuteReader();
+            table = new DataTable();
+            table.Load(reader);
+            
+            reader.Close();
+        }
+        catch (Exception exp)
+        {
+            //TODO: LogError
+            throw exp;
+        }
+        finally
+        {            
+            command.Connection.Close();
+        }
+        return table;
+    }
+    
+    public static DbCommand CreateCommand()
+    {        
+        string dataProviderName = UniversityChatConfiguration.DbProviderName;        
+        string connectionString = UniversityChatConfiguration.DbConnectionString;
+        
+        DbProviderFactory factory = DbProviderFactories.GetFactory(dataProviderName);
+        
+        DbConnection conn = factory.CreateConnection();       
+        conn.ConnectionString = connectionString;
+        
+        DbCommand comm = conn.CreateCommand();        
+        comm.CommandType = CommandType.StoredProcedure;
+        
+        return comm;
+    }
+}
---
+++ UniversityChat-SignalR-vs2010/UniversityChat.Data/DataAccess/UniversityChatConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System.Configuration;
+
+public static class UniversityChatConfiguration
+{
+    private static string dbConnectionString;
+    private static string dbProviderName;
+
+    static UniversityChatConfiguration()
+    {
+        dbConnectionString = @"";
+        dbProviderName = @"";
+    }
+
+    public static string DbConnectionString
+    {
+        get
+        {
+            return dbConnectionString;
+        }
+    }
+
+    public static string DbProviderName
+    {
+        get
+        {
+            return dbProviderName;
+        }
+    }
+}
