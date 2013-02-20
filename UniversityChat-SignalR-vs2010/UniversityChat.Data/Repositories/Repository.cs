09c28db0de29e061fb9d5f8b4220c5b4b627d954
ahmsal1984 <ahmsal19@uw.edu>
2013-02-19 19:22:59 -0800

    Added Repository Interface. Added UserRepository; not fully implemented.

---
+++ UniversityChat-SignalR-vs2010/UniversityChat.Data/Repositories/Repository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace UniversityChat.Data.Repositories
+{
+    public interface Repository<T>
+    {
+        bool Create(T item);
+
+        bool Delete(T item);
+
+        IList<T> GetAll();
+
+        T GetById(decimal id);
+
+        T GetByCriteria(T item);
+
+        bool Update(T item);
+    }
+}
---
+++ UniversityChat-SignalR-vs2010/UniversityChat.Data/Repositories/UserRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using UniversityChat.Model;
+
+namespace UniversityChat.Data.Repositories
+{
+    public class UserRepository: Repository<User>
+    {
+        public bool Create(User item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(User item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(decimal id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(User item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetByCriteria(User item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
