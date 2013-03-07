1ec5c4864df6b340ab3278a3bfda895e7199eb5e
Shane H <shaneh425@gmail.com>
2013-03-06 23:34:50 -0800

    All Layouts supported & user # count per chat room

---
+++ University Chat Android Client/src/com/universitychat/androidclient/fragments/ChatMemberList.java
@@ -0,0 +1,50 @@
+package com.universitychat.androidclient.fragments;
+
+import com.universitychat.androidclient.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class ChatMemberList extends Fragment
+{
+	public ListView memberList;
+	public String[] chatMemberArray = {};//{"Loading..."};
+	private TextView numUsers;
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+    }
+	
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) 
+    {
+    	View v = inflater.inflate(R.layout.fragment_chat_member_list, container,false);
+    	memberList = (ListView)v.findViewById(R.id.list);
+    	memberList.setAdapter(new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1, chatMemberArray));
+    	numUsers = (TextView) v.findViewById(R.id.textView_num_users);
+        
+        return v;
+    }
+	
+	@Override
+	public void onPause()
+	{
+		super.onPause();	
+	}
+	
+	public void setChatMemberList(String[] newMemberList)
+	{
+		memberList.setAdapter(new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1, newMemberList));
+		numUsers.setText(Integer.toString(newMemberList.length));
+		//numUsers.setText("11");
+	}
+}
+
