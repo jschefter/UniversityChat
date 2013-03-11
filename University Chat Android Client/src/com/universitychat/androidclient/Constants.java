6f0b5767b342a09f9b30a21e87baade7037e5898
Darren Korman <darrenkorman@hotmail.com>
2013-03-11 00:48:25 -0700

    setting Android to log on live site.

--- University Chat Android Client/src/com/universitychat/androidclient/Constants.java
+++ University Chat Android Client/src/com/universitychat/androidclient/Constants.java
@@ -1,7 +1,7 @@
 package com.universitychat.androidclient;
 
 public class Constants {
-	public static final String DEFAULT_LOGIN = "http://ucbeta.azurewebsites.net/AndroidLogin.aspx";//"http://universitychat.azurewebsites.net/AndroidLogin.aspx";
-    public static final String DEFAULT_HOST = "http://ucbeta.azurewebsites.net/Android.html"; // "http://universitychat.azurewebsites.net/Android.html";
+	public static final String DEFAULT_LOGIN = "http://universitychat.azurewebsites.net/AndroidLogin.aspx";
+    public static final String DEFAULT_HOST = "http://universitychat.azurewebsites.net/Android.html";
     public static final String FEEDBACK_URL = "https://docs.google.com/forms/d/1pav-_eoF8V522xKQuj0tjt-dTFSGxPsFZ9ct_ZE9ylg/viewform";
 }
