a646d6de123736220a22ae7589489dcbc5952970
Shane H <shaneh425@gmail.com>
2013-03-10 22:57:08 -0700

    Additional Android Features Added

--- University Chat Android Client/src/com/universitychat/androidclient/LoginWindow.java
+++ University Chat Android Client/src/com/universitychat/androidclient/LoginWindow.java
@@ -16,6 +16,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -27,6 +28,7 @@ public class LoginWindow extends Activity
 	private EditText passwordText;
 	private Button loginButton;
 	private Button loginAnonymousButton;
+	private CheckBox saveUserChkbx;
 	private TextView signUpLink;
 	private AlertDialog.Builder builder;
 	private AlertDialog dialog;
@@ -42,8 +44,7 @@ public class LoginWindow extends Activity
 		TextView tv = (TextView) findViewById(R.id.uchatheader);
 		tv.setTypeface(orbitron);
 		newHostURL = null;	
-	}
-	
+	}	
 	
 	private void setUIVariables() 
     {
@@ -52,6 +53,7 @@ public class LoginWindow extends Activity
         loginButton = (Button)findViewById(R.id.btn_login);
         loginAnonymousButton = (Button)findViewById(R.id.btn_loginAnonymous);
         signUpLink = (TextView)findViewById(R.id.textView_signuplink);
+        saveUserChkbx = (CheckBox)findViewById(R.id.chkbx_user_save_login);
     }
 
 	@Override
@@ -117,7 +119,6 @@ public class LoginWindow extends Activity
 	{
 		Intent signUpIntent = new Intent(this, SignupActivity.class);
 		startActivity(signUpIntent);
-		
 	}
 	
 	public void loginAttempt(View view)
--- University Chat Android Client/src/com/universitychat/androidclient/SignupActivity.java
+++ University Chat Android Client/src/com/universitychat/androidclient/SignupActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
