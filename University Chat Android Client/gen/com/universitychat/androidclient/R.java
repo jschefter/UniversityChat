02903b5eede3f1abef66365fee32c241cb587af2
Shane H <shaneh425@gmail.com>
2013-03-14 01:52:12 -0700

    Possible Android Final Release
    1)Remove Channel & Kill App Menu buttons for debug removed
    2) About Menu Modified
    3) Room/Member list appearance changed
    4)Escape characters fixed
    5)Toast and System out debug calls removed
    6)Search Rooms deleted
    7)Create Channel disabled until join room
    8)Signup page redirection to website added
    9) Keyboard auto capitalize disabled on log in
    10) Launcher Icon Added
    
    Same issues remain from earlier with android screeen orientation, no
    supported anonymous login, No 2G-4G connection.

--- University Chat Android Client/gen/com/universitychat/androidclient/R.java
+++ University Chat Android Client/gen/com/universitychat/androidclient/R.java
@@ -13,6 +13,7 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int roundedbutton=0x7f020001;
+        public static final int uchat_launcher=0x7f020002;
     }
     public static final class id {
         public static final int Button01=0x7f07000a;
